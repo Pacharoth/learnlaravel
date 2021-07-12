530ed3814242efc9b7c677041cdcf0d86c4d65c8
roeun <pacharothroeun01@gmail.com>
2021-07-12 20:04:50 +0700

    [Task] Authentication completed

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,11 +15,27 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//application/json
 //create model php artisan make:model NameModel --migration
-//create Controller php artisan make:controller NameController --api for api
+//create Controller php artisan make:controller NameController
 // php artisan route:list
-Route::resource('products',ProductController::class);
+
+//Public route
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
 Route::get('/products/search/{name}',[ProductController::class,'search']);
+Route::get('/products/{id}',[ProductController::class,'show']);
+Route::get('/products',[ProductController::class,'index']);
+
+//Protected Route
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/{id}',[ProductController::class,'update']);
+    Route::delete('/products/{id}',[ProductController::class,'destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
